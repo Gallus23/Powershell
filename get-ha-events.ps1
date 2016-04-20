0f5cd84c91da160b3f2174324f088dd624f4af0b
Gallus23 <mike@thehowards.org.uk>
2016-04-20 13:49:13 +0100

    get-ha-events.ps1 lists vm HA events in 72hours

---
+++ get-ha-events.ps1
@@ -0,0 +1,171 @@
+﻿#--------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Lists HA events for the last 
+#--------------------------------------------------------------------------------------------------------------------------------------
+#Variable declaration
+param(
+	[string]$Location
+	)
+
+add-pssnapin -name VMware.VimAutomation.Core
+
+function Get-VIEventPlus {
+<#   
+.SYNOPSIS  Returns vSphere events    
+.DESCRIPTION The function will return vSphere events. With
+    the available parameters, the execution time can be
+   improved, compered to the original Get-VIEvent cmdlet. 
+.NOTES  Author:  Luc Dekens   
+.PARAMETER Entity
+   When specified the function returns events for the
+   specific vSphere entity. By default events for all
+   vSphere entities are returned. 
+.PARAMETER EventType
+   This parameter limits the returned events to those
+   specified on this parameter. 
+.PARAMETER Start
+   The start date of the events to retrieve 
+.PARAMETER Finish
+   The end date of the events to retrieve. 
+.PARAMETER Recurse
+   A switch indicating if the events for the children of
+   the Entity will also be returned 
+.PARAMETER User
+   The list of usernames for which events will be returned 
+.PARAMETER System
+   A switch that allows the selection of all system events. 
+.PARAMETER ScheduledTask
+   The name of a scheduled task for which the events
+   will be returned 
+.PARAMETER FullMessage
+   A switch indicating if the full message shall be compiled.
+   This switch can improve the execution speed if the full
+   message is not needed.   
+.EXAMPLE
+   PS> Get-VIEventPlus -Entity $vm
+.EXAMPLE
+   PS> Get-VIEventPlus -Entity $cluster -Recurse:$true
+#>
+ 
+  param(
+    [VMware.VimAutomation.ViCore.Impl.V1.Inventory.InventoryItemImpl[]]$Entity,
+    [string[]]$EventType,
+    [DateTime]$Start,
+    [DateTime]$Finish = (Get-Date),
+    [switch]$Recurse,
+    [string[]]$User,
+    [Switch]$System,
+    [string]$ScheduledTask,
+    [switch]$FullMessage = $false
+  )
+ 
+  process {
+    $eventnumber = 100
+    $events = @()
+    $eventMgr = Get-View EventManager
+    $eventFilter = New-Object VMware.Vim.EventFilterSpec
+    $eventFilter.disableFullMessage = ! $FullMessage
+    $eventFilter.entity = New-Object VMware.Vim.EventFilterSpecByEntity
+    $eventFilter.entity.recursion = &{if($Recurse){"all"}else{"self"}}
+    $eventFilter.eventTypeId = $EventType
+    if($Start -or $Finish){
+      $eventFilter.time = New-Object VMware.Vim.EventFilterSpecByTime
+    if($Start){
+        $eventFilter.time.beginTime = $Start
+    }
+    if($Finish){
+        $eventFilter.time.endTime = $Finish
+    }
+    }
+  if($User -or $System){
+    $eventFilter.UserName = New-Object VMware.Vim.EventFilterSpecByUsername
+    if($User){
+      $eventFilter.UserName.userList = $User
+    }
+    if($System){
+      $eventFilter.UserName.systemUser = $System
+    }
+  }
+  if($ScheduledTask){
+    $si = Get-View ServiceInstance
+    $schTskMgr = Get-View $si.Content.ScheduledTaskManager
+    $eventFilter.ScheduledTask = Get-View $schTskMgr.ScheduledTask |
+      where {$_.Info.Name -match $ScheduledTask} |
+      Select -First 1 |
+      Select -ExpandProperty MoRef
+  }
+  if(!$Entity){
+    $Entity = @(Get-Folder -Name Datacenters)
+  }
+  $entity | %{
+      $eventFilter.entity.entity = $_.ExtensionData.MoRef
+      $eventCollector = Get-View ($eventMgr.CreateCollectorForEvents($eventFilter))
+      $eventsBuffer = $eventCollector.ReadNextEvents($eventnumber)
+      while($eventsBuffer){
+        $events += $eventsBuffer
+        $eventsBuffer = $eventCollector.ReadNextEvents($eventnumber)
+      }
+      $eventCollector.DestroyCollector()
+    }
+    $events
+  }
+}
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+switch ($Location.ToLower())
+    {
+    "lo3" {$ManvCenter = "lo3wpvcdvcs002.dcsprod.dcsroot.local"}
+    "lo1" {$ManvCenter = "lo1wpvcdvcs002.dcsprod.dcsroot.local"}
+    "icd" {$ManvCenter = "icdwpvcdvcs002.dcsprod.dcsroot.local"}
+    "nj2" {$ManvCenter = "nj2wpvcdvcs002.dcsprod.dcsroot.local"}
+    "bz1" {$ManvCenter = "bz1wpvcdvcs002.dcsprod.dcsroot.local"}
+    "sg8" {$ManvCenter = "sg8wpvcdvcs002.dcsprod.dcsroot.local"}
+    "sh1" {$ManvCenter = "sh1wpvcdvcs002.dcsprod.dcsroot.local"}
+    "bj1" {$ManvCenter = "bj1wpvcdvcs002.dcsprod.dcsroot.local"}
+    "au1" {$ManvCenter = "au1wpvcdvcs002.dcsprod.dcsroot.local"}
+     }
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+#Test connection to destination vCenter
+
+write-host "-Date and time is: $((Get-Date).ToString())"
+
+Write-Host "Testing Connection to vCenter $ManvCenter" -foregroundcolor "magenta" 
+if(!(Test-Connection -Cn $ManvCenter -BufferSize 16 -Count 1 -ea 0 -quiet))
+{
+write-host "Connection to $ManvCenter failed cannot ping" -foregroundcolor "red" 
+$Global:lasterror = "Connection to $ManvCenter failed cannot ping"
+}
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+#Connect to vCenter
+Write-Host "Connecting to vCenter $ManvCenter" -foregroundcolor "yellow"     
+
+try
+{
+    Connect-VIServer -Server  $ManvCenter  -ErrorAction Stop | Out-Null
+}
+catch 
+{
+    Write-Host "failed to connect to vCenter. Error is $_"
+    $Global:lasterror = $_
+}
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+$entity = Get-Folder Datacenters
+$start = (Get-Date).Adddays(-3)
+$Recurse = $false
+$eventTypes = "com.vmware.vc.ha.VmRestartedByHAEvent"
+ 
+Get-VIEventPlus -Entity $entity -Start $start -EventType $eventTypes | Select CreatedTime,@{N="VM";E={$_.Vm.Name}},@{N="ESX";E={$_.Host.Name}}
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+#Disconnect the vCenter
+write-host "-Date and time is: $((Get-Date).ToString())"
+
+write-host "Disconnecting vSphere $ManvCenter......."
+disconnect-viserver -server $ManvCenter -Confirm:$false -force
